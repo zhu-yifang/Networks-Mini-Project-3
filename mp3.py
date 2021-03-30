79f6c38b8732a3549e497ab423ee0f0870b883e5
zhu-yifang <zhuyifang@reed.edu>
2021-03-29 23:27:55 -0700

    rdt 2.1 Tx
    finished rdt 2.1 for Tx

--- mp3.py
+++ mp3.py
@@ -23,29 +23,50 @@ def send(sock: socket.socket, data: bytes):
         data -- A bytes object, containing the data to send over the network.
     """
 
-    # rdt 2.0
+    # rdt 2.1
     logger = miniproject3.logging.get_logger("mp3-sender")
     chunk_size = miniproject3.MAX_PACKET - 5
-    pause = .1
+    pause = 0.1
     offsets = range(0, len(data), miniproject3.MAX_PACKET)
     for chunk in [data[i:i + chunk_size] for i in offsets]:
-        while True:
-            # wait for call from above
-            sock.send(b'00000' + chunk)
-            logger.info("Pausing for %f seconds", round(pause, 2))
-            time.sleep(pause)
-            # Wait for NAC or ACK
-            ACK = sock.recv(1000).decode("utf-8")
-            # if (rdt_rcv(rcvpkt) && isACK(rcvpkt))
-            print(ACK)
-            if ACK == '00000':
-                # send the next chunk
-                break
-            # elif (rdt_rcv(rcvpkt) && isNAK(rcvpkt))
-            else:
-                # resend
-                pass
-        
+        sequence = 00000
+        if sequence == 00000:
+            while True:
+                # wait for call 0 from above
+                # rdt_send(data)
+                sock.send(str(sequence).encode() + chunk)
+                logger.info("Pausing for %f seconds", round(pause, 2))
+                time.sleep(pause)
+                # Wait for NAC or ACK 0
+                ACK = sock.recv(1000).decode("utf-8")
+                # if (rdt_rcv(rcvpkt) && isACK(rcvpkt))
+                if ACK == '00000':
+                    # send the next chunk
+                    sequence = 00001
+                    break
+                # elif (rdt_rcv(rcvpkt) && isNAK(rcvpkt))
+                else:
+                    # resend()
+                    pass
+        # sequence == 00001
+        else:
+            while True:
+                # wait for call 1 from above
+                # rdt_send(data)
+                sock.send(str(sequence).encode() + chunk)
+                logger.info("Pausing for %f seconds", round(pause, 2))
+                time.sleep(pause)
+                # Wait for NAC or ACK 1
+                ACK = sock.recv(1000).decode("utf-8")
+                # if (rdt_rcv(rcvpkt) && isACK(rcvpkt))
+                if ACK == '00001':
+                    # send the next chunk
+                    sequence = 00000
+                    break
+                # elif (rdt_rcv(rcvpkt) && isNAK(rcvpkt))
+                else:
+                    # resend()
+                    pass       
 
 
 
