80479543ec9c090db6d1ddd8328c7b6d7a3d4ea0
zhu-yifang <zhuyifang@reed.edu>
2021-03-30 01:17:09 -0700

    rdt 2.1 buggy version
    There is a problem in this version, Dropping buffer is full happens all the time. Need to debug

--- mp3.py
+++ mp3.py
@@ -29,12 +29,12 @@ def send(sock: socket.socket, data: bytes):
     pause = 0.1
     offsets = range(0, len(data), miniproject3.MAX_PACKET)
     for chunk in [data[i:i + chunk_size] for i in offsets]:
-        sequence = 00000
-        if sequence == 00000:
+        sequence = '00000'
+        if sequence == '00000':
             while True:
                 # wait for call 0 from above
                 # rdt_send(data)
-                sock.send(str(sequence).encode() + chunk)
+                sock.send(sequence.encode() + chunk)
                 logger.info("Pausing for %f seconds", round(pause, 2))
                 time.sleep(pause)
                 # Wait for NAC or ACK 0
@@ -42,7 +42,7 @@ def send(sock: socket.socket, data: bytes):
                 # if (rdt_rcv(rcvpkt) && isACK(rcvpkt))
                 if ACK == '00000':
                     # send the next chunk
-                    sequence = 00001
+                    sequence = '00001'
                     break
                 # elif (rdt_rcv(rcvpkt) && isNAK(rcvpkt))
                 else:
@@ -53,7 +53,7 @@ def send(sock: socket.socket, data: bytes):
             while True:
                 # wait for call 1 from above
                 # rdt_send(data)
-                sock.send(str(sequence).encode() + chunk)
+                sock.send(sequence.encode() + chunk)
                 logger.info("Pausing for %f seconds", round(pause, 2))
                 time.sleep(pause)
                 # Wait for NAC or ACK 1
@@ -61,7 +61,7 @@ def send(sock: socket.socket, data: bytes):
                 # if (rdt_rcv(rcvpkt) && isACK(rcvpkt))
                 if ACK == '00001':
                     # send the next chunk
-                    sequence = 00000
+                    sequence = '00000'
                     break
                 # elif (rdt_rcv(rcvpkt) && isNAK(rcvpkt))
                 else:
@@ -83,17 +83,46 @@ def recv(sock: socket.socket, dest: io.BufferedIOBase) -> int:
         The number of bytes written to the destination.
     """
     logger = miniproject3.logging.get_logger("mp3-receiver")
-    # rdt 2.0
+    # rdt 2.1
     num_bytes = 0
-    # Wait for call from below
+    # set state to 0
+    state = '00000'
     while True:
         data = sock.recv(miniproject3.MAX_PACKET)
         if not data:
             break
+        # get the sequnce number
+        sequence = data[0:5]
+        # wait for 0 from below
+        if state == '00000':
+            while True:
+                # has_seq0()
+                if sequence == state:
+                    # ACK && change state && deliver
+                    sock.send(state.encode())  # ACK
+                    state = '00001'   # change state to 1
+                    break   # deliver
+                # has_seq1()
+                else:
+                    # ACK
+                    sock.send(state.encode())
+        # wait for 1 from below
+        # if state == '00001':
+        else:
+            while True:
+                # has_seq1()
+                if sequence == state:
+                    # ACK && change state && deliver
+                    sock.send(state.encode())  # ACK
+                    state = '00000'   # change state to 0
+                    break   # deliver
+                # has_seq0()
+                else:
+                    # ACK
+                    sock.send(state.encode())
+        # deliver            
         logger.info("Received %d bytes", len(data))
-        dest.write(data)
+        dest.write(data[5:])
         num_bytes += len(data)
-        # send ACK
-        sock.send(b'00000')
         dest.flush()
     return num_bytes
